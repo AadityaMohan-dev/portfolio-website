ddd1fa8d5cf793157d8bcade2ce80d4dbe767d65
AadityaMohan <aadimohan51@gmail.com>
2026-01-04 16:11:09 +0530

    some changes

--- src/component/Dashboard.tsx
+++ src/component/Dashboard.tsx
@@ -2,14 +2,14 @@ import About from "./About";
 import Home from "./Home";
 import Project from "./Project";
 import SlideSection from "./SlideSection";
-import bg from "../assets/HomeBackground.svg";
-import Contact from "./Contact"
+import Contact from "./Contact";
 import Navbar from "./Navbar";
+import bg from "../assets/HomeBackground.svg";
 
 function Dashboard() {
   return (
     <div
-      className="min-h-[300vh] bg-gray-50"
+      className="relative min-h-[300vh] bg-gray-50"
       style={{
         backgroundImage: `url(${bg})`,
         backgroundRepeat: "no-repeat",
@@ -17,38 +17,37 @@ function Dashboard() {
         backgroundPosition: "center",
       }}
     >
-      <div className="fixed flex justify-center w-full"><Navbar/></div>
-      {/* Home stays in normal flow */}
-        <SlideSection z = {5}>
-          
-          <Home/>
-        </SlideSection>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6">
-        
-        {/* Slides over Home */}
+      {/* FIXED NAVBAR (DO NOT WRAP IN EXTRA DIVS) */}
+      <Navbar />
+
+      {/* HOME (BASE LAYER) */}
+      <SlideSection z={5}>
+        <Home />
+      </SlideSection>
+
+      {/* SLIDING CONTENT */}
+      <div className="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        {/* ABOUT */}
         <SlideSection z={10}>
-          <div className="bg-white/90 border rounded-xl px-4 sm:px-6 lg:px-8 py-6">
-             <About />
+          <div className="rounded-xl border bg-white/90 px-4 py-6 sm:px-6 lg:px-8">
+            <About />
           </div>
-         
         </SlideSection>
 
-        {/* Slides over About */}
+        {/* PROJECTS */}
         <SlideSection z={20}>
-          <div className=" bg-[#F78154]   border rounded-xl px-10 text-white py-10">
+          <div className="rounded-xl border bg-[#F78154] px-4 py-8 text-white sm:px-6 lg:px-10">
             <Project />
           </div>
-          
         </SlideSection>
-        <SlideSection z={20}>
-          <div className="bg-zinc-900  border rounded-xl px-10 text-white py-10">
+
+        {/* CONTACT */}
+        <SlideSection z={30}>
+          <div className="rounded-xl border bg-zinc-900 px-4 py-10 text-white sm:px-6 lg:px-10">
             <Contact />
           </div>
-          
         </SlideSection>
-        
       </div>
-      
     </div>
   );
 }
